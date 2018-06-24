4525f3b91062fa1ffdf2960ff512f97d41e0300a
Vidit7792 <39307865+Vidit7792@users.noreply.github.com>
2018-06-24 14:18:46 +0530

    Add files via upload

---
+++ javascript2.1.js
@@ -0,0 +1,26 @@
+a=-8;
+b=12.5;
+c=-6.769;
+d=0;
+e=-1;
+
+if (a>b && a>c && a>d && a>e)
+{
+    console.log(a)
+}
+else if (b>a && b>c && b>d && b>e)
+{
+    console.log(b)
+}
+else if (c>a && c>b && c>d && c>e)
+{
+    console.log(c)
+}
+else if (d>a && d>c && d>b && d>e)
+{
+    console.log(d)
+}
+else
+{
+    console.log(e)
+}
